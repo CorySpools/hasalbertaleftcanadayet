9870f4a801bda398990cc218319276b5cd771299
CorySpools <40255044+CorySpools@users.noreply.github.com>
2026-05-27 00:14:18 -0700

    Set to "No"

--- src/App.js
+++ src/App.js
@@ -12,37 +12,19 @@ export default function App() {
     <div className="app min-h-screen text-blue-200 flex items-center flex-col p-20">
       <div className="mb-10 grid grid-cols-4 grid-rows-2 w-1/2 mx-auto">
         <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img
+        <h1>
           className="col-span-2 row-span-3 animate-spin m-auto"
           style={{ animationDuration: "30s" }}
           src={logo}
           alt="React Logo"
           width="300"
-        />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-      </div>
-
-      <h1 className="text-2xl lg:text-5xl mb-10 text-right">
-        Welcome to Your New React App{" "}
-        <span className="block text-lg text-blue-400">on DigitalOcean</span>
-      </h1>
-
-      <div className="grid grid-cols-2 grid-rows-2 gap-4">
-        <Button
-          text="DigitalOcean Docs"
-          url="https://www.digitalocean.com/docs/app-platform"
-        />
-        <Button
-          text="DigitalOcean Dashboard"
-          url="https://cloud.digitalocean.com/apps"
-        />
+        </h1>
       </div>
     </div>
   );
 }
 
+/*
 function Button({ className, text, url = "#" }) {
   return (
     <a
@@ -53,3 +35,4 @@ function Button({ className, text, url = "#" }) {
     </a>
   );
 }
+  */
