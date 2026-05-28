a4ea1693ebd06820087932bd4830a083f046ebe1
CorySpools <40255044+CorySpools@users.noreply.github.com>
2026-05-28 01:05:03 -0700

    remove div.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import "./App.css";
 export default function App() {
   return (
     <div className="app min-h-screen text-blue-200 flex items-center flex-col p-20">
-      <div className="mb-10 grid grid-cols-4 grid-rows-2 w-1/2 mx-auto">
         <h1
           className="animate-spin m-auto"
           style={{ animationDuration: "5s" }}
@@ -18,7 +17,6 @@ export default function App() {
         >
         No
         </h1>
-      </div>
     </div>
   );
 }
